7efee645d1d4d50053b885e64fc5e1fd75d66067
unknown <muhammad.hamza@aitomation.com>
2025-09-16 12:11:37 +0500

    some updates regarding the posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="blog-home"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
     path("post/new/", views.PostCreateView.as_view(), name="post-create"),
     path("about/", views.about, name="blog-about"),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # from django.http import HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.views.generic import (
     CreateView,
@@ -41,14 +41,20 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ["title", "content"]
-    template_name = "blog/post_update.html"  # <app>/<model>_<viewtype>.html
+    # template_name = "blog/post_form.html"  # <app>/<model>_<viewtype>.html
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
